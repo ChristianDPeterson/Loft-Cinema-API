7cea18721edc666cf04b131366857e63a4f043b3
Christian Peterson <cdpeterson@email.arizona.edu>
2022-07-31 12:21:22 -0700

    feat: use new website

--- index.js
+++ index.js
@@ -14,112 +14,103 @@ dayjs.extend(utc);
 async function getShowtimes(url) {
 	const { data } = await axios.get(encodeURI(url));
 	const $ = cheerio.load(data);
+	const showtimeHTMLElements = Array.from($("h3 > a"));
 
-	const showtimeHTMLElements = Array.from($(".movielist .movie-info-box"));
-	const allShowtimes = await showtimeHTMLElements.reduce(async (prev, el) => {
-		const cinema = $(".fav-h1 h1").text().trim();
-		const movieElement = $(el).find(".media-heading a").attr("title");
-		const movie = movieElement
-			? movieElement.replace(" info", "")
-			: $(el).find(".media-heading").text().trim();
-
-		const movieDetails = await queryMovie(movie);
-		const runtime = movieDetails?.runtime ?? 120;
-		const description = movieDetails?.overview ?? "No description found";
-
-		const timeString = $(el).find(".buttonticket").text();
-		const showtimes = parseTimes(timeString).map((time) => {
-			return { cinema, movie, time, runtime, description };
+	const showtimes = showtimeHTMLElements
+		.map((showtime) => {
+			return showtime.attribs.href;
+		})
+		.filter((url, index, self) => {
+			return self.indexOf(url) === index;
 		});
 
-		return [...(await prev), ...showtimes];
-	}, []);
-
-	console.log(allShowtimes);
-	return allShowtimes;
-}
-
-async function queryMovie(title) {
-	// change into query string
-	const queryString = title?.split(" ").join("+").toLowerCase();
-
-	// get movieId from themoviedb
-	const { data: movieQuery } = await axios.get(
-		encodeURI(
-			`https://api.themoviedb.org/3/search/movie?api_key=${process.env.TMDB_API_KEY}&language=en-US&query=${queryString}&page=1`
-		)
+	const times = await Promise.all(
+		showtimes.map(async (showtime) => {
+			return await getInformation(showtime);
+		})
 	);
 
-	if (!movieQuery || !movieQuery.results || movieQuery.results.length === 0) {
-		return null;
+	const filteredTimes = times.filter((time) => {
+		return time !== null;
+	});
+
+	return filteredTimes;
+}
+
+async function getInformation(url) {
+	const { data } = await axios.get(encodeURI(url));
+	const $ = cheerio.load(data);
+	const title = $(".image-header .container h1").text().trim();
+	const description = $(".film-content p").text().trim();
+
+	const runtimeString = $(".film-extras h4").text().trim();
+	let runtime =
+		parseInt(runtimeString.split(" ")[0]) * 60 +
+		parseInt(runtimeString.split(" ")[2]);
+	if (isNaN(runtime)) {
+		runtime = 120;
 	}
 
-	const id = movieQuery.results[0].id;
+	const showtimeLink = $(".showtime-link").prop("href");
+	if (!showtimeLink) {
+		return null;
+	}
+	const showtimes = await parseTimes("https://loftcinema.org" + showtimeLink);
 
-	// get details on movie
-	const { data: movieDetail } = await axios.get(
-		encodeURI(
-			`https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.TMDB_API_KEY}&language=en-US`
-		)
-	);
-
-	return movieDetail ? movieDetail : null;
+	return {
+		title,
+		description,
+		runtime,
+		showtimes,
+		url,
+	};
 }
 
-function parseTimes(timeString) {
-	const dates = timeString
-		.split(/Sun, |Mon, |Tue, |Wed, |Thu, |Fri, |Sat, /)
-		.filter(Boolean); // remove empty strings
-
-	const showtimes = dates.reduce((prev, date) => {
-		const month = date.substring(0, date.indexOf(" "));
-		const day = date.substring(date.indexOf(" ") + 1, date.indexOf(":"));
-		const timeList = date.substring(date.indexOf(":") + 1);
-
-		const times = timeList
-			.split(/(?<=m)/) // split on "m" from "am" or "pm"
-			.filter(Boolean) // remove empty strings
-			.map((time) => {
-				const dateString = `${month} ${day} ${time}`;
-				const dateObject = dayjs
-					.utc(dateString, "MMM D hh:mma")
-					.utcOffset(7);
-				return dateObject;
-			});
-
-		return [...prev, ...times];
-	}, []);
-
-	return showtimes;
-}
-
-function generateEvents(showtimes) {
-	return showtimes.map((showtime) => {
-		const startTime = [
-			showtime.time.year(),
-			showtime.time.month() + 1,
-			showtime.time.date(),
-			showtime.time.hour(),
-			showtime.time.minute(),
-		];
-
-		return {
-			calName: "Loft Cinema",
-			title: showtime.movie,
-			description: showtime.description,
-			start: startTime,
-			duration: {
-				hours: Math.floor(showtime.runtime / 60),
-				minutes: showtime.runtime % 60,
-			},
-		};
+async function parseTimes(url) {
+	const { data } = await axios.get(encodeURI(url));
+	const $ = cheerio.load(data);
+	const days = Array.from($(".date-collection > .selectable-date"));
+	const times = days.map((time) => {
+		const timeString = $(time).attr("data-date").split(" @ ");
+		const dateObject = dayjs
+			.utc(timeString[0] + " " + timeString[1], "ddd M/D H:mma")
+			.utcOffset(7);
+		return dateObject;
 	});
+
+	return times;
+}
+
+function generateEvents(movies) {
+	return movies.reduce((acc, movie) => {
+		const events = movie.showtimes.map((showtime) => {
+			const startTime = [
+				showtime.year(),
+				showtime.month() + 1,
+				showtime.date(),
+				showtime.hour(),
+				showtime.minute(),
+			];
+
+			return {
+				calName: "Loft Cinema",
+				title: movie.title,
+				description: movie.description,
+				start: startTime,
+				url: movie.url,
+				geo: { lat: 32.236467, lon: -110.923583 },
+				duration: {
+					hours: Math.floor(movie.runtime / 60),
+					minutes: movie.runtime % 60,
+				},
+			};
+		});
+		return [...acc, ...events];
+	}, []);
 }
 
 async function main() {
-	const showtimes = await getShowtimes(
-		"https://www.showtimes.com/movie-theaters/loft-cinema-12077/?date=all"
-	);
+	const showtimes = await getShowtimes("https://loftcinema.org/showtimes/");
 	const events = generateEvents(showtimes);
 	ics.createEvents(events, (error, value) => {
 		if (error) {
