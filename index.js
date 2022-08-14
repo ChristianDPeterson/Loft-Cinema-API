ba94a8aa9c774538f8a19eb1153df96b5c4513f6
Christian Peterson <cdpeterson@email.arizona.edu>
2022-08-13 19:54:22 -0700

    remove console log

--- index.js
+++ index.js
@@ -159,7 +159,6 @@ function formatEvents(movies) {
 async function main() {
 	const showtimes = await getShowtimes("https://loftcinema.org/showtimes/");
 	const events = formatEvents(showtimes);
-	console.log(events);
 	ics.createEvents(events, (error, value) => {
 		if (error) {
 			console.log(error);
