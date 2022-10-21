b2764a91c8fd0a2dc2e578cd9c320ace50d215b1
Christian Peterson <cdpeterson@email.arizona.edu>
2022-10-21 15:54:56 -0700

    fix: remove log

--- src/index.ts
+++ src/index.ts
@@ -10,8 +10,6 @@ async function main() {
 	const browser = await chromium.puppeteer.launch();
 	const events = await getEvents(browser);
 
-	console.dir(events);
-
 	ics.createEvents(events, (error, value) => {
 		if (error) {
 			console.log(error);
